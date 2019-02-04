793e8abec02cbf50a1aaba16fff7f341dc04c9ac
Jessy Hu <jesshuy12@gmail.com>
2019-02-04 01:39:56 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -73,7 +73,7 @@ def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
   holiday_name_array = []
-    holiday_hash.
+    holiday_hash.each do |
   
 
 end
