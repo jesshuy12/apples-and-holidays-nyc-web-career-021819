544f6de800168bd4163dd43c05d0f8af1e88764c
Jessy Hu <jesshuy12@gmail.com>
2019-02-04 02:05:23 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -70,7 +70,7 @@ def all_supplies_in_holidays(holiday_hash)
   holiday_hash.each do |season, holiday|
     puts "#{season.to_s.capitalize!}:"
     holiday.each do |holiday, supply|
-      puts "#{holiday.to_s.split(" ").collect{|value| value.capitalize} "
+      puts "#{holiday.to_s.split(" ").collect{|value| value.capitalize}.join "
 
 end
 
